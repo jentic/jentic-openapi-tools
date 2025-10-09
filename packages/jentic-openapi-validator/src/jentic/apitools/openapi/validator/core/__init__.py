f5bc07bfa98f723d93b7de95d71a2b1ad97d3f9d
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-10-09 10:00:15 +0200

    refactor: use relative imports

--- packages/jentic-openapi-validator/src/jentic/apitools/openapi/validator/core/__init__.py
+++ packages/jentic-openapi-validator/src/jentic/apitools/openapi/validator/core/__init__.py
@@ -1,5 +1,5 @@
-from jentic.apitools.openapi.validator.core.diagnostics import ValidationResult
-from jentic.apitools.openapi.validator.core.openapi_validator import OpenAPIValidator
+from .diagnostics import ValidationResult
+from .openapi_validator import OpenAPIValidator
 
 
 __all__ = ["ValidationResult", "OpenAPIValidator"]
