5efe73a712af053a4f2a2f061629a2405f2c0b9a
Vladimír Gorej <vladimir@jentic.com>
2025-10-15 17:52:03 +0200

    refactor(parser): consolidate parser backends (#74)

--- packages/jentic-openapi-parser/src/jentic/apitools/openapi/parser/backends/ruamel_safe.py
+++ packages/jentic-openapi-parser/src/jentic/apitools/openapi/parser/backends/ruamel_safe.py
@@ -1,9 +1,8 @@
-import json
 import logging
 from collections.abc import Sequence
-from typing import Any, Literal, Mapping
+from typing import Literal, Mapping
 
-from ruamel.yaml import YAML
+from ruamel.yaml import YAML, CommentedMap
 
 from jentic.apitools.openapi.parser.backends.base import BaseParserBackend
 from jentic.apitools.openapi.parser.core.uri import is_uri_like, load_uri
@@ -17,39 +16,11 @@ class RuamelSafeParserBackend(BaseParserBackend):
         self.yaml = YAML(typ=typ, pure=pure)
         self.yaml.default_flow_style = False
 
-    def parse(self, document: str, *, logger: logging.Logger | None = None) -> Any:
+    def parse(self, document: str, *, logger: logging.Logger | None = None) -> CommentedMap:
         logger = logger or logging.getLogger(__name__)
-        text = document
-        try:
-            if is_uri_like(document):
-                logger.debug("Starting download of %s", document)
-                text = load_uri(document, 5, 10, logger)
-
-            data = self.parse_text(text)
-        except Exception:
-            msg = f"Unsupported document type: {type(document)!r}"
-            logger.exception(msg)
-            raise TypeError(msg)
-        return data
-
-    def parse_uri(self, uri: str, logger: logging.Logger | None = None) -> Any:
-        return self.parse_text(load_uri(uri, 5, 10, logger))
-
-    def parse_text(self, text: str, logger: logging.Logger | None = None) -> Any:
-        logger = logger or logging.getLogger(__name__)
-        try:
-            data = self.yaml.load(text)
-            logger.debug("loaded YAML")
-        except Exception:
-            if isinstance(text, (bytes, str)):
-                text = text.decode() if isinstance(text, bytes) else text
-                data = json.loads(text)
-                logger.debug("loaded JSON")
-        if isinstance(data, Mapping):
-            return data
-        msg = f"Unsupported document type: {type(text)!r}"
-        logger.error(msg)
-        raise TypeError(msg)
+        if is_uri_like(document):
+            return self._parse_uri(document, logger)
+        return self._parse_text(document, logger)
 
     @staticmethod
     def accepts() -> Sequence[Literal["uri", "text"]]:
@@ -61,3 +32,22 @@ class RuamelSafeParserBackend(BaseParserBackend):
             - "text": String (JSON/YAML) representation
         """
         return ["uri", "text"]
+
+    def _parse_uri(self, uri: str, logger: logging.Logger) -> CommentedMap:
+        logger.debug("Starting download of %s", uri)
+        return self._parse_text(load_uri(uri, 5, 10, logger), logger)
+
+    def _parse_text(self, text: str, logger: logging.Logger) -> CommentedMap:
+        if not isinstance(text, (bytes, str)):
+            raise TypeError(f"Unsupported document type: {type(text)!r}")
+
+        if isinstance(text, bytes):
+            text = text.decode()
+
+        data: CommentedMap = self.yaml.load(text)
+        logger.debug("YAML document successfully parsed")
+
+        if not isinstance(data, Mapping):
+            raise TypeError(f"Parsed YAML document is not a mapping: {type(data)!r}")
+
+        return data
